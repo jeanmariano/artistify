06246922d65823f554319b53a10981d602909fdf
jeanmariano <jeandmariano@gmail.com>
2015-12-11 22:52:50 -0500

    idk why u want this but here

---
+++ script.js
@@ -0,0 +1,34 @@
+function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('clock').innerHTML =
+        h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+
+function toggleView(id) {
+    if ($('#'+id+'View').css('display') === 'none') {
+       $('#'+id+'View').css('display','block');
+    }
+    else
+        $('#'+id+'View').css('display','none');
+}
+
+function goToView(from,to) {
+    toggleView(from);
+    toggleView(to);
+}
+
+$(document).ready(function() {
+    startTime()
+    checkTime()        
+    $('#sleepView').css('display','none');    
+});
