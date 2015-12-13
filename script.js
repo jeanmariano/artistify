87a6446c8098999d922c9b2a920b396f761d33cc
jeanmariano <jeandmariano@gmail.com>
2015-12-13 00:04:57 -0500

    calls sleepy music fn

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var selectGenres = [];
+var queue = [];
+var audio = new Audio();
 function startTime() {
         var today = new Date();
         var h = today.getHours();
@@ -26,6 +29,57 @@ function goToView(from,to) {
     toggleView(to);
 }
 
+function toggleActive(obj) {
+    if ($(obj).hasClass('active'))
+        $(obj).removeClass('active');
+    else
+        $(obj).addClass('active');
+}
+
+function sleepNow() {
+    // getWakeySongs(display,selectGenres)
+    goToView('sleep','main');
+    $('#genreList .list-group-item').removeClass('active');
+    selectGenres = [];
+    playMusic();
+}
+
+function cancelAlarm() {
+    goToView('main','sleep');
+    audio.pause();
+}
+
+function display(list) {
+    console.log(list)
+}
+
+function queueSongs(list) {
+    queue = queue.concat(list);
+}
+    
+// }
+function playMusic() {
+    $('#albumCover').attr('src',queue[0].album_image);
+    $('#albumName').text(queue[0].album_name)
+    $('#artistName').text(queue[0].artist_name)
+    $('#songName').text(queue[0].track_name);    
+    audio = new Audio(queue[0].preview_url);
+    audio.play();
+    audio.addEventListener('ended',function(){
+        queue.splice(0,1);
+        audio.src = queue[0].preview_url;
+        audio.pause();
+        audio.load();
+        audio.play();
+        $('#albumCover').attr('src',queue[0].album_image);
+        $('#albumName').text(queue[0].album_name)
+        $('#artistName').text(queue[0].artist_name)
+        $('#songName').text(queue[0].track_name);
+    }); 
+}
+
+getSleepySongs(queueSongs);
+
 $(document).ready(function() {
     startTime()
     checkTime()
@@ -33,4 +87,10 @@ $(document).ready(function() {
     $('#alarmView').css('display','none');
     $('#snoozeView').css('display','none');
     $('#playlistView').css('display','none');
+
+    $('#genreList').on('click','.list-group-item',function(e) {
+        selectGenres.push(e.target.text);
+        toggleActive(e.target);
+    });
+
 });
