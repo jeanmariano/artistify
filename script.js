96be26c0b8c621eb603ceda415962c5103ec17b0
jeanmariano <jeandmariano@gmail.com>
2015-12-13 01:11:10 -0500

    renders playlist view

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function toggleActive(obj) {
 }
 
 function sleepNow() {
-    // getWakeySongs(display,selectGenres)
+    getWakeySongs(queuePlaylist,selectGenres);
     goToView('sleep','main');
     $('#genreList .list-group-item').removeClass('active');
     selectGenres = [];
@@ -47,14 +47,68 @@ function sleepNow() {
 function cancelAlarm() {
     goToView('main','sleep');
     audio.pause();
+    queue = [];
+    getSleepySongs(queueSongs);
 }
 
 function display(list) {
     console.log(list)
 }
 
+function queuePlaylist(list) {
+    renderPlaylist(list);
+    queueSongs(list);
+}
+
 function queueSongs(list) {
     queue = queue.concat(list);
+    console.log(queue);
+}
+
+function renderPlaylist(list) {    
+    var html = '';
+    for (var i=0; i < list.length; i++) {
+        var cur = list[i];
+        var albumCover =cur.album_image,
+            song = cur.track_name,
+            artist = cur.artist_name,
+            artistId = '',
+            albumName = cur.album_name,
+            previewUrl = cur.preview_url,
+            trackId = '';
+        html = html+renderPlaylistEntry(albumCover, song, artist, '1vCWHaC5f2uS3yhpwWbIA6', albumName, previewUrl, trackId);
+    }
+    $('#playlist').empty();
+    $('#playlist').append(html);
+}
+
+function renderPlaylistEntry(albumCover, song, artist, artistId, albumName, previewUrl, trackId) {
+    var html = '<li class="musicplayer list-group-item text-left">'+
+      '<div class="albumcover">'+
+        '<img src="'+albumCover+'">'+
+      '</div>'+
+      '<div class="info">'+
+        '<span class="title">'+song+'</span><br>'+
+        '<div>'+
+          '<b style="display:inline-block; height:25px;">'+artist+'</b>'+
+          '<iframe src="https://embed.spotify.com/follow/1/?uri=spotify:artist:'+artistId+'&size=basic&theme=light&show-count=0" width="200" height="25" scrolling="no" frameborder="0" style="border:none; overflow:hidden; display:inline-block;" allowtransparency="true"></iframe>'+
+        '</div>'+
+        '<br>'+  
+        '<i>'+albumName+'</i><br>'+
+        '<button class="play btn btn-primary">'+
+          '<span class="glyphicon glyphicon-play" aria-hidden="true"></span>'+
+          'Preview Track'+
+          '<audio class="audioDemo" controls="" preload="none">'+
+          '<source src="'+previewUrl+'" type="audio/mpeg">'+
+          '</audio>'+
+        '</button> '+
+        '<a class="openinspotify btn btn-default" href="https://play.spotify.com/track/'+trackId+'" target="_blank">'+
+          '<span class="glyphicon glyphicon-link" aria-hidden="true"></span>'+
+          'Open in Spotify'+
+        '</a>'+
+      '</div>'+
+    '</li>';
+    return html;
 }
     
 // }
