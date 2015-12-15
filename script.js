5973a6608dc6f6b02f019a116b9192bc360c038a
jeanmariano <jeandmariano@gmail.com>
2015-12-15 03:24:28 -0500

    maaad updates to the UI

--- script.js
+++ script.js
@@ -4,67 +4,74 @@ var selectGenres = [],
     sleepyAudio = new Audio(),
     wakeyAudio = new Audio(),
     samplerAudio = new Audio(),
-    alarmTime = '',
-    timeLeft = '';
+    alarmTime = {'hour': 0, 'minute': 0, 'seconds': 0},
+    timeLeft = {'hour': 0, 'minute': 0, 'seconds': 0};
 
 var counter = 1;
 
 function startTime() {
-    var today = new Date();
-    var h = today.getHours(),
-        m = today.getMinutes(),
-        s = today.getSeconds(),
-        p = '';
-    m = checkTime(m);
-    s = checkTime(s);
-    if (h > 12) {
-      h = h - 12;
-    }
-    if (h <= 12) {
-      p = 'AM';
-    }
-    else {
-      p = 'PM'
-    }
-    $('.clock').text(h + ":" + m + " " + p);
-    var t = setTimeout(startTime, 500);
+  var today = new Date();
+  var h = today.getHours(),
+      m = today.getMinutes(),
+      s = today.getSeconds(),
+      p = '';
+  m = checkTime(m);
+  s = checkTime(s);
+  if (h <= 12) {
+    p = 'AM';
+  }
+  else {
+    p = 'PM'
+  }
+  if (h > 12) {
+    h = h - 12;
+  }
+  $('.clock').text(h + ":" + m + "" + p);
+  var t = setTimeout(startTime, 500);
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
 }
 
 function toggleView(id) {
-    if ($('#'+id+'View').css('display') === 'none') {
-       $('#'+id+'View').css('display','block');
-    }
-    else
-        $('#'+id+'View').css('display','none');
+  toggleDisplay(id+'View');
+}
+
+function toggleDisplay(id) {
+  if ($('#'+id).css('display') === 'none') {
+    $('#'+id).css('display','block');
+  }
+  else
+    $('#'+id).css('display','none');
 }
 
 function goToView(from,to) {
-    toggleView(from);
-    toggleView(to);
+  toggleView(from);
+  toggleView(to);
 }
 
 function toggleActive(obj) {
-    if ($(obj).hasClass('active'))
-        $(obj).removeClass('active');
-    else
-        $(obj).addClass('active');
+  if ($(obj).hasClass('active'))
+    $(obj).removeClass('active');
+  else
+    $(obj).addClass('active');
 }
 
 function sleepNow() {
-    getWakeySongs(queuePlaylist,selectGenres);
-    goToView('alarmMusic','main');
-    $('#genreList .list-group-item').removeClass('active');
-    selectGenres = [];
-    playSleepyMusic();
-    startTime()
-    checkTime()
+  initializeMainView() 
+  getWakeySongs(queuePlaylist,selectGenres);
+  goToView('alarmMusic','main');
+  initializeGenres()
+  console.log(selectGenres);    
+  playSleepyMusic();
+  startTime()
+  checkTime()
     // get current time
     // timeLeft = alarmTime - timeOut
+  var deadline = new Date(Date.now() + 24 * 60 * 60 * 1000);
+  initializeClock('clockdiv', deadline); 
 }
 
 function wakeUp() {
@@ -85,13 +92,14 @@ function snoozeAlarm() {
   setTimeout(function() {
     wakeyAudio.play();
     goToView('alarm','snooze');
-  },500);
+  },2500);
 }
 
-function cancelAlarm() {
-  goToView('main','alarmMusic');
+function cancelAlarm(to,from) {
+  goToView('menu',from);
   sleepyAudio.pause();
-  // there won't be a point that will a user will want to cancel during an alarm  
+  wakeyAudio.pause();
+  // for testing purposes only-- there won't be a point that will a user will want to cancel during an alarm  
   sleepyQueue = [];
   getSleepySongs(queueSleepylist);  
   sleepyAudio.volume = 1;
@@ -122,15 +130,15 @@ function queueSleepylist(list) {
 function renderPlaylist(list) {
   var html = '';
   for (var i=0; i < list.length; i++) {
-      var cur = list[i];
-      var albumCover =cur.album_image,
-          song = cur.track_name,
-          artist = cur.artist_name,
-          artistId = cur.artist_id,
-          albumName = cur.album_name,
-          previewUrl = cur.preview_url,
-          trackId = cur.track_id;
-      html = html+renderPlaylistEntry(albumCover, song, artist, artistId, albumName, previewUrl, trackId);
+    var cur = list[i];
+    var albumCover =cur.album_image,
+        song = cur.track_name,
+        artist = cur.artist_name,
+        artistId = cur.artist_id,
+        albumName = cur.album_name,
+        previewUrl = cur.preview_url,
+        trackId = cur.track_id;
+    html = html+renderPlaylistEntry(albumCover, song, artist, artistId, albumName, previewUrl, trackId);
   }
   $('#playlist').empty();
   $('#playlist').append(html);
@@ -256,21 +264,118 @@ function stopAllTracks() {
 }
 
 function initializeAlarmView() {
-    $('#sleepView').css('display','block');
-    $('#alarmView').css('display','none');
-    $('#snoozeView').css('display','none');
+  $('#sleepView').css('display','block');
+  $('#alarmView').css('display','none');
+  $('#snoozeView').css('display','none');
 }
+
+function initializeMainView() {
+  $('#page1').css('display','block');
+  $('#page2').css('display','none');
+}
+
+function initializeGenres() {
+  $('#genreList').css('display','none');
+  $('#specifyNo').addClass('btn-primary');
+  $('#specifyYes').removeClass('btn-primary');
+  $('#genreList .list-group-item').removeClass('active');
+  selectGenres = [];  
+}
+
+function next() {
+  toggleDisplay('page1');
+  toggleDisplay('page2');
+  alarmTime.hour = $('#selectHours').val();
+  alarmTime.minute = $('#selectMin').val();
+  console.log(alarmTime);
+}
+
+function back() {
+  toggleDisplay('page1');
+  toggleDisplay('page2');
+}
+
+function showGenres(bool) {
+  if (bool) {
+    $('#genreList').css('display','block');
+    $('#specifyYes').addClass('btn-primary');
+    $('#specifyNo').removeClass('btn-primary');
+  }
+  else {
+    $('#genreList').css('display','none');
+    genreList = [];
+    $('#specifyNo').addClass('btn-primary');
+    $('#specifyYes').removeClass('btn-primary');
+  }
+}
+
+function createAlarm() {
+  goToView('main','menu');
+}
+
+// countdown
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.now();
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+// end countdown
 
 getSleepySongs(queueSleepylist);
 
 $(document).ready(function() {    
-    $('#alarmMusicView').css('display','none');
-    initializeAlarmView();
-    $('#playlistView').css('display','none');
+  $('#alarmMusicView').css('display','none');    
+  $('#playlistView').css('display','none');
+  initializeAlarmView();
+  initializeMainView();
+  initializeGenres();
+  $('#mainView').css('display','none');
+  
+  $('#genreList').on('click','.list-group-item',function(e) {
+      selectGenres.push(e.target.text);
+      toggleActive(e.target);
+  });
 
-    $('#genreList').on('click','.list-group-item',function(e) {
-        selectGenres.push(e.target.text);
-        toggleActive(e.target);
-    });
+  $('#timepicker1').timepicker();
 
+  for (i=1;i<=12;i++) {
+    $('#selectHours').append($('<option></option>').val(i).html(i));
+  }
+  for (i=1;i<=60;i++) {
+    var j = i;
+    if (i < 10) {
+      j = '0'+i;
+    }
+    $('#selectMin').append($('<option></option>').val(j).html(j));
+  }
+  for (i=1;i<=20;i++) {
+    $('#snoozeDrop').append($('<option></option>').val(i+' min').html(i+' min'));
+  }
 });
